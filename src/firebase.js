74f8072b068f8ef1f987b54cb7c3f08274aa4de5
Duncan Moyo <duncanfmoyo@gmail.com>
2020-08-12 18:15:08 +0200

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfUJyPEU8yvsiQ_JIUs30Q9xdBdaQweak",
+  authDomain: "mcity-d137d.firebaseapp.com",
+  databaseURL: "https://mcity-d137d.firebaseio.com",
+  projectId: "mcity-d137d",
+  storageBucket: "mcity-d137d.appspot.com",
+  messagingSenderId: "537745669509",
+  appId: "1:537745669509:web:b5551c658f8de56ad618ec",
+  measurementId: "G-HJ5LC58HP9",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const firebaseDB = firebase.database();
+
+firebaseDB.ref('matches').once('value').then(snapshot => {
+  console.log(snapshot.val());
+})
