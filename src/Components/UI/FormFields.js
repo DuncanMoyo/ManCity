21b2257c9f61ac6ea8005cfc6678664b12805c52
Duncan Moyo <duncanfmoyo@gmail.com>
2020-08-16 22:55:38 +0200

    validation message through CSS

--- src/Components/UI/FormFields.js
+++ src/Components/UI/FormFields.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React from "react";
 
-const FormFields = ({formData, id, change}) => {
+const FormFields = ({ formData, id, change }) => {
+  const showError = () => {
+    let errorMessage = (
+      <div className="error_label">
+        {formData.validation && !formData.valid
+          ? formData.validationMessage
+          : null}
+      </div>
+    );
+
+    return errorMessage;
+  };
 
   const renderTemplate = () => {
-    let formTemplate = null
+    let formTemplate = null;
 
-    switch(formData.element) {
-      case('input'):
+    switch (formData.element) {
+      case "input":
         formTemplate = (
           <div>
-            <input {...formData.config} value={formData.value} onChange={event => change({event, id})}/>
+            <input
+              {...formData.config}
+              value={formData.value}
+              onChange={(event) => change({ event, id })}
+            />
+            {showError()}
           </div>
-        )
-      break;
-      default: formTemplate = null
+        );
+        break;
+      default:
+        formTemplate = null;
     }
-    return formTemplate
-  }
+    return formTemplate;
+  };
 
-  return (
-    <div>
-      {renderTemplate()}
-    </div>
-  );
+  return <div>{renderTemplate()}</div>;
 };
 
 export default FormFields;
