1081ba5008cad348457b5ba6a04490678637a89d
Duncan Moyo <duncanfmoyo@gmail.com>
2020-08-18 17:27:22 +0200

    auth creating a dashboard

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import Layout from "./Hoc/Layout";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import SignIn from "./Components/SignIn";
+import Dashboard from "./Components/Admin/Dashboard";
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>
